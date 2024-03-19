4e5464f14ada0a2ba1110067560e3fe8f8ae5148
bling-yshs <377178599@qq.com>
2024-03-19 11:32:46 +0800

    docs: update Chinese printing

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,15 @@ use std::path::Path;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: quick_delete <path>");
+        eprintln!("Usage(CMD): fast-delete-rs path");
         return;
     }
     let path = &args[1];
     if Path::new(path).is_dir() {
-        println!("你正在删除一个文件夹，请按回车确认删除...");
+        println!("You are deleting a folder, please press enter to confirm deletion...你正在删除一个文件夹，请按回车确认删除...");
         io::stdin().read_line(&mut String::new()).unwrap();
     }
+    println!("Deleting...删除中...");
     let path_vec = vec![path];
     fs_extra::remove_items(&path_vec).unwrap();
 }
