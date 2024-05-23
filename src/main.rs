512f07ed1037bd66a92677d7af128f6ba15791e1
bling-yshs <377178599@qq.com>
2024-05-23 22:10:50 +0800

    feat: Show path

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,14 @@ fn main() {
     }
     let path = &args[1];
     if Path::new(path).is_dir() {
-        println!("You are deleting a folder, please press enter to confirm deletion...你正在删除一个文件夹，请按回车确认删除...");
+        println!("Current path: {}", path);
+        println!("当前路径: {}", path);
+        println!("You are deleting a folder, please press enter to confirm deletion...");
+        println!("你正在删除一个文件夹，请按回车确认删除...");
         io::stdin().read_line(&mut String::new()).unwrap();
     }
-    println!("Deleting...删除中...");
+    println!("Deleting...");
+    println!("删除中...");
     let path_vec = vec![path];
     fs_extra::remove_items(&path_vec).unwrap();
 }
